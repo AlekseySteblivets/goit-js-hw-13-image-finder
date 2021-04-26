18334ca13518b2c1412593801701051c3262efe6
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-04-26 20:59:31 +0300

    сохраняет класс

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './styles.css';
-import fetchFotosPixabay from './js/apiService';
+import NewApiService  from './js/apiService';
 
-console.log(fetchFotosPixabay('running'));
+const fetchFotosPixabay = NewApiService();
+
+console.log(fetchFotosPixabay);
+
+// const refs = {
+//     searchForm: document.querySelector('form[id=search-form]'),
+
+// }
+
+// console.log(refs.searchForm);
+
+// refs.searchForm.addEventListener('submit', onSearch);
+
+// function onSearch (e) {
+//     e.preventDefault();
+
+//     fetchFotosPixabay.query = e.currentTarget.elements.query.value;
+//     fetchFotosPixabay.fetchFotos();
+// }
+
+// console.log(fetchFotosPixabay(1, 'running'));
 
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,40 @@
 
-export default function fetchPixabay(searchQuery) {
-    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=21303972-574e9d18be62e9d74443b9e84`).then(res => {
-     if(!res.ok) {
-         throw res;
-     }
-     return res.json();
- });
- }
+// export default function fetchPixabay(page = 1, searchQuery) {
+//     return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=21303972-574e9d18be62e9d74443b9e84`).then(res => {
+//      if(!res.ok) {
+//          throw res;
+//      }
+//      return res.json();
+//  });
+//  }
  
+export default class NewApiService {
+    constructor () {
+        this.searchQuery = '';
+        this.page = 1;
+
+    }
+
+    fetchFotos() {
+        // console.log(this);
+        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=21303972-574e9d18be62e9d74443b9e84`;
+        fetch(url)
+        .then(res => {
+            if(!res.ok) {
+                throw res;
+            }
+            return res.json();
+             })
+        .then (data => {
+            this.page += 1
+        });
+    }
+
+    get query () {
+        return this.searchQuery;
+    }
+
+    set query (newQuery) {
+        this.searchQuery = newQuery;
+    }
+};
